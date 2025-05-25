04b7602d2e42c0cd901420a3bacf636036146b08
singhanshu04 <singhanshu0409@gmail.com>
2025-05-25 20:33:30 +0530

    Create my.sql

---
+++ ProjectStructure/model/Database/my.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE chat_app;
+
+USE chat_app;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL
+);
